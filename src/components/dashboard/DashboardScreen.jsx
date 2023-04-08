3bc09f7c112296a6724c82f2cfe209969913b9d4
Daniel <danielmuniz902@gmail.com>
2023-04-08 18:15:13 +0100

    Fix sidebar styles

--- src/components/dashboard/DashboardScreen.jsx
+++ src/components/dashboard/DashboardScreen.jsx
@@ -11,27 +11,33 @@ import { FaProjectDiagram } from "react-icons/fa";
 import inicio from "../../assets/inicio.svg";
 
 const DashboardScreen = () => {
+  // cont menuItemStyle
   const { collapsed } = useProSidebar();
 
   return (
     <div className="dashboard_wrapper">
       <div className="sidebar">
-        <Sidebar width="280px">
-          <Menu>
-            <MenuItem
-              style={{
-                padding: "2rem",
-                marginTop: "1rem",
-                width: "100%",
-              }}
-            >
-              {collapsed ? <span>INICIO</span> : <img src={inicio} alt="inicioLogo" />}
-            </MenuItem>
-            <SubMenu label="Seguridad" icon={<MdLock />}>
+        <Sidebar width="260px" transitionDuration={500}>
+          <Menu
+            transitionDuration={500}
+            menuItemStyles={{
+              button: ({ level, active, disabled }) => {
+                // only apply styles on first level elements of the tree
+                if (level === 0 || level === 1)
+                  return {
+                    color: disabled ? "rgba(0, 0, 0, 0.54);" : "rgba(0, 0, 0, 0.54)",
+                    backgroundColor: active ? "#eecef9" : undefined,
+                  };
+              },
+            }}
+          >
+            {collapsed ? <h1 className="dashboard_collapsed_logo">INC</h1> : <img src={inicio} alt="inicioLogo" className="dashboard_logo" />}
+            {/* </MenuItem> */}
+            <SubMenu label="Seguridad" icon={<MdLock className="menuItem" />}>
               <MenuItem component={<Link to="user" />}> Usuarios </MenuItem>
             </SubMenu>
-            <SubMenu label="Nomencladores" icon={<IoMdSettings />}>
-              <MenuItem component={<Link to="area" />}> Área </MenuItem>
+            <SubMenu label="Nomencladores" icon={<IoMdSettings className="menuItem" />}>
+              <MenuItem component={<Link to="area" />}>Área</MenuItem>
               <MenuItem component={<Link to="unitmeasure" />}> Unidad de Medida </MenuItem>
               <MenuItem component={<Link to="supplier" />}> Proveedores </MenuItem>
               <MenuItem component={<Link to="product_category" />}>Categoría del producto</MenuItem>
@@ -46,21 +52,21 @@ const DashboardScreen = () => {
               <MenuItem component={<Link to="subcontract" />}> Subcontrataciones </MenuItem>
               <MenuItem component={<Link to="roles" />}> Cargos </MenuItem>
             </SubMenu>
-            <SubMenu label="Recursos Humanos" icon={<MdPeople />}>
+            <SubMenu label="Recursos Humanos" icon={<MdPeople className="menuItem" />}>
               <MenuItem component={<Link to="worker" />}> Trabajador </MenuItem>
             </SubMenu>
-            <SubMenu label="Oficina" icon={<HiOfficeBuilding />}>
+            <SubMenu label="Oficina" icon={<HiOfficeBuilding className="menuItem" />}>
               <MenuItem component={<Link to="office_expenses" />}> Gastos de oficina </MenuItem>
             </SubMenu>
-            <SubMenu label="Almacén" icon={<FaWarehouse />}>
+            <SubMenu label="Almacén" icon={<FaWarehouse className="menuItem" />}>
               <MenuItem component={<Link to="warehouse" />}> Almacén </MenuItem>
               <MenuItem component={<Link to="tickets_warehouse" />}> Almacén de vales </MenuItem>
             </SubMenu>
-            <SubMenu label="Proyecto" icon={<FaProjectDiagram />}>
+            <SubMenu label="Proyecto" icon={<FaProjectDiagram className="menuItem" />}>
               <MenuItem component={<Link to="projects" />}> Proyectos </MenuItem>
               <MenuItem component={<Link to="project_expenses" />}> Gastos de proyectos </MenuItem>
             </SubMenu>
-            <MenuItem component={<Link to="reports" />} icon={<IoMdDocument />}>
+            <MenuItem component={<Link to="reports" />} icon={<IoMdDocument className="menuItem" />}>
               Reportes
             </MenuItem>
           </Menu>
