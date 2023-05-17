09101ddedf8369b2b02268acc75a198536ae8722
Daniel <danielmuniz902@gmail.com>
2023-05-17 16:18:45 +0100

    Add tests for login

--- src/actions/auth.test.jsx
+++ src/actions/auth.test.jsx
@@ -1,20 +1,28 @@
-import { describe, expect, it } from "vitest";
-import { login, logout, usersLoaded, selectedUser, startLogin, startLogout } from "./auth";
+import { beforeEach, describe, expect, it, vitest } from "vitest";
+import { startLogin } from "./auth";
 import { types } from "../types/types";
 import thunk from "redux-thunk";
-
 import configureStore from "redux-mock-store";
+import Swal from "sweetalert2";
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
 
-const store = mockStore({
+const initState = {
   auth: {
     logged: "false",
   },
-});
+};
+
+let store = mockStore(initState);
+
+Storage.prototype.setItem = vitest.fn();
 
 describe("Tests in auth and users actions", () => {
+  beforeEach(() => {
+    store = mockStore(initState);
+    vitest.clearAllMocks();
+  });
   const user = {
     user: "dmuniz",
     password: "123456",
@@ -25,40 +33,30 @@ describe("Tests in auth and users actions", () => {
       password: "123456",
     },
   ];
-  it("should all actions be functional", async () => {
-    const loginAction = login(user);
-    const logoutAction = logout();
-    const usersLoadedAction = usersLoaded(users);
-    const selectedUserAction = selectedUser(user);
 
-    expect(loginAction).toEqual({
+  it("Correct login", async () => {
+    await store.dispatch(startLogin("dmuniz", "123456"));
+    const actions = store.getActions();
+
+    expect(actions[0]).toEqual({
       type: types.login,
       payload: {
-        user,
+        user: expect.any(String),
       },
     });
+    expect(localStorage.setItem).toHaveBeenCalledWith("token", expect.any(String));
+    expect(localStorage.setItem).toHaveBeenCalledWith("token-init-date", expect.any(Number));
+  });
 
-    expect(logoutAction).toEqual({
-      type: types.logout,
-    });
+  it("Incorrect login", async () => {
+    await store.dispatch(startLogin("dmuniz", "asdasd456789"));
+    let actions = store.getActions();
 
-    expect(usersLoadedAction).toEqual({
-      type: types.usersLoaded,
-      payload: users,
-    });
+    expect(actions).toEqual([]);
 
-    expect(selectedUserAction).toEqual({
-      type: types.selectedUser,
-      payload: user,
-    });
+    await store.dispatch(startLogin("test", "123456"));
+    actions = store.getActions();
 
-    await store.dispatch(startLogin(user.user, user.password));
-
-    await store.dispatch(startLogout());
-
-    const storeState = store.getState();
-    const actions = store.getActions();
-    console.log(storeState);
-    console.log(actions);
+    expect(actions).toEqual([]);
   });
 });
